d2af436a1a3b714dc36f73f1cfbf6db398c72b10
JohnsonJavaweb <andachemicalssale@gmail.com>
2022-11-18 15:56:04 +0800

    Test submit

--- Test.java
+++ Test.java
@@ -14,6 +14,8 @@ public class Test {
 		System.out.println("a-- = " + (a--));
 		System.out.println("d++ = " + (d++));
 		System.out.println("++d = " + (++d));
+		System.out.println("d-- = " + (d--));
+		System.out.println("++a = " + (++a));
 		
 	}
 } 
