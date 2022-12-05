edc9fc5898cf129d53ecdf3e8ac60f1c73a7c34f
JohnsonJavaweb <andachemicalssale@gmail.com>
2022-12-05 18:24:39 +0800

    submit 2022.12.5 2nd

---
+++ BitOperator02.java
@@ -0,0 +1,12 @@
+public class BitOperator02 {
+	public static void main(String[] args) {
+		System.out.println(1 >> 2);
+		System.out.println(1 << 2);
+		System.out.println(-1 >> 2);//-1
+		System.out.println(-1 << 2);//-4
+		System.out.println(3 >>> 2);//0
+		int i=66;
+		System.out.println(++i+i);
+		
+	}
+}
